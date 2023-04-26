6620bdb3b7af6c37d04c9b149b4cb7de9a5a61b5
javier aldana <jdad1701@gmail.com>
2023-04-26 10:35:29 +0200

    explicacion clase

--- main.js
+++ main.js
@@ -7,4 +7,30 @@ const registeredEmails = [
 
 // PASO PREVIO: Añadir un listener al formulario para el event 'submit', y ejecutar el event.preventDefault()
 
+document
+  .querySelector("#formulario-registro")
+  .addEventListener("submit", function (event) {
 
+    event.preventDefault();
+        
+    const ocultar = document.querySelector("#mensaje")
+    ocultar.classList.remove('hidden');
+
+    const email = document.querySelector('input').value
+
+    if (registeredEmails.includes(email)) {
+      
+    document.querySelector("#mensaje > p").textContent =
+      "El usuario con el correo " + email + " ya existe.";
+    document.querySelector("input").value = "";
+    
+  } else {
+    
+    document.querySelector("form").style.display = "none";
+    document.querySelector("#mensaje > p").textContent =
+      "Gracias por registrarte, te avisaremos cuando abramos! ";
+
+  };
+
+
+});
